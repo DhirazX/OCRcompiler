439f9fc142b3b46b693c36463bb48af692f0312e
Dhiraj <dhrjpoudyal@gmail.com>
2023-07-23 18:58:36 +0545

    Image section done

--- Client/src/sections/ImageSection.jsx
+++ Client/src/sections/ImageSection.jsx
@@ -8,31 +8,30 @@ function imageInput({ onchangeHandler, selectedImage }) {
       <div className="section-title">
         <div className="section-title-text">IMAGE</div>
       </div>
-      <div className="image-input-wrapper">
-        <form
-          action=""
-          onClick={() => document.querySelector(".imgInputBox").click()}
-          className="image-input"
-        >
-          <div className="image-input-text">
-            <AiOutlineCloudUpload className="upload-icon" />
-            <p>Click to Upload</p>
-          </div>
-          <input
-            type="file"
-            accept="image/*"
-            className="imgInputBox"
-            onChange={onchangeHandler}
-            hidden
-          ></input>
-          <div className="image-view">
-            .
-            {selectedImage && (
+      <form action="" className="image-input">
+        <div className="image-input-hero">
+          {selectedImage ? (
+            <div className="image-display">
               <img src={URL.createObjectURL(selectedImage)} id="inputImg"></img>
-            )}
-          </div>
-        </form>
-      </div>
+            </div>
+          ) : (
+            <div
+              className="image-input-text"
+              onClick={() => document.querySelector(".imgInputBox").click()}
+            >
+              <AiOutlineCloudUpload className="upload-icon" />
+              <p>Click to Upload</p>
+            </div>
+          )}
+        </div>
+        <input
+          type="file"
+          accept="image/*"
+          className="imgInputBox"
+          onChange={onchangeHandler}
+          hidden
+        ></input>
+      </form>
     </div>
   );
 }
