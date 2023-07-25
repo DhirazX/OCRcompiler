bc38398a34630e32d7afda1804f236a068ff5a8d
Dhiraj <dhrjpoudyal@gmail.com>
2023-07-25 22:22:05 +0545

    Code Editor Added

--- Client/src/sections/OcrSection.jsx
+++ Client/src/sections/OcrSection.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { AiOutlineScan } from "react-icons/ai";
+import CodeMirror from "@uiw/react-codemirror";
+import { cpp } from "@codemirror/lang-cpp";
+import { dracula } from "@uiw/codemirror-theme-dracula";
 
 function OcrSection() {
   return (
@@ -10,7 +13,18 @@ function OcrSection() {
           OCR
         </div>
       </div>
-      <div className="code-editor">Code</div>
+      <div className="code-editor">
+        <CodeMirror
+          value="console.log('hello world!');
+          #include<iostream.h>
+          int a,b;
+          for(i=0;i<12;i++){}"
+          height="100%"
+          theme={dracula}
+          extensions={[cpp({ cpp: true })]}
+          className="codemirror-wrapper"
+        />
+      </div>
       <div className="ocr-btns">
         <div className="compile-btn btn-primary">Compile</div>
         <div className="prettify-btn btn-secondary">Prettify</div>
